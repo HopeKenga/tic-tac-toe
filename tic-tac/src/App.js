07d4fbcc513709546b8638e556bc42a8406efaa7
Hope Kenga <97263417+HopeKenga@users.noreply.github.com>
2022-04-17 18:48:14 +0300

    css

---
+++ tic-tac/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import board from './Board.js';
+
+function App() {
+    return <div className = "Board" >
+
+        <
+        /div>
+};
+
+export default App;
---
+++ tic-tac/src/components/Board.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './Board.css'
+
+function Board(props) {
+    return ( <
+        div >
+        This is a Board <
+        /div>
+    )
+};
+
+export default Board;
